d04d4524486bb4e99e90a1c45973e6b81e03cf82
zf58 <sreerag.ravindran@in.tesco.com>
2018-05-07 02:28:52 +0530

    try with multiple api keys

--- stock.js
+++ stock.js
@@ -1,5 +1,6 @@
 function PriceVolumeData(timestamp, open, high, low, close, volume){
     this.timestamp = timestamp;
+  
     this.open = open;
     this.high = high;
     this.low = low;
@@ -12,70 +13,102 @@ function Stock(symbol){
     this.priceVolumeSeries = []; 
 }
 
-function StockExchange(){
-    const alpha = require("alphavantage")({key : '3FUAI09JYAO19WCX'}); 
-    this.data = alpha.data;   
-}
+class AlphaVantage {
+    constructor(apiKey) {
+        const alpha = require("alphavantage")({ key: apiKey });
+        this.data = alpha.data;
+    }
 
-StockExchange.prototype.getAllStocks = function(){
-    var stocks = [];
-    var fs = require('fs');
-    var symbols = fs.readFileSync('equities.txt').toString().split("\n");
-    symbols.forEach(s => {
-        stocks.push(new Stock(s));
-    })
-    return stocks;    
-}
+    getAllStocks() {
+        var stocks = [];
+        var fs = require('fs');
+        var symbols = fs.readFileSync('equities.txt').toString().split("\n");
+        symbols.forEach(s => {
+            stocks.push(new Stock(s));
+        });
+        return stocks;
+    }
 
-StockExchange.prototype.getIntraday1MSeriesFor = function(stock, callback){
-    
-    this.data.intraday(stock.symbol,'compact','json','1min')
-        .then(data => {
-            var timeSeries = data['Time Series (1min)']; 
-            for(var key in timeSeries){
-                if(timeSeries.hasOwnProperty(key)){
-                    stock.priceVolumeSeries.push(new PriceVolumeData(
-                        key,                     
-                        timeSeries[key]['1. open'],
-                        timeSeries[key]['2. high'],
-                        timeSeries[key]['3. low'],
-                        timeSeries[key]['4. close'],
-                        timeSeries[key]['5. volume']
-                    ))
+    /**
+     *
+     * @param {Stock} stock
+     * @param {function} callback
+     */
+    getIntraday1mSeriesForAStock(stock, callback) {
+        this.data.intraday(stock.symbol, 'compact', 'json', '1min')
+            .then(data => {
+                var timeSeries = data['Time Series (1min)'];
+                for (var key in timeSeries) {
+                    if (timeSeries.hasOwnProperty(key)) {
+                        stock.priceVolumeSeries.push(new PriceVolumeData(
+                            key, 
+                            timeSeries[key]['1. open'], 
+                            timeSeries[key]['2. high'], 
+                            timeSeries[key]['3. low'], 
+                            timeSeries[key]['4. close'], 
+                            timeSeries[key]['5. volume'])
+                        );
+                    }
                 }
-            }
-            return callback(null, stock);                 
-        })        
-        .catch((error) => {
-            return callback({stock : stock, errorMessage: error});
-        })  
+                return callback(null, stock);
+            })
+            .catch((error) => {
+                return callback({ stock: stock, errorMessage: error });
+            });
+    }
 
+    // encapsulates the logic to regulate the calls to api for multiple stocks 
+    // ensures a request is fired once every nth second 
+    getIntraday1mSeriesForAllStocks(callback) {
+        var stocks = this.getAllStocks();
+        var index = 0;
+        // hold a reference to the stock exchange object to be used inside the closure instead of 'this'    
+        var alphaVantage = this;
+        (function getData() {
+            setTimeout(() => {
+                if (index < stocks.length) {
+                    alphaVantage.getIntraday1MSeriesFor(stocks[index], callback);
+                    index++;
+                    getData();
+                }
+            }, 3000);
+        })();
+    }
+
+    /**
+     *
+     * @param {Array[Stocks]} stocks
+     * @param {function} callback
+     */
+    getIntraday1mSeriesForStocks(stocks, callback) {
+        if (stocks && stocks.length > 0) {
+            var index = 0;
+            var alphaVantage = this;
+            (function getData() {
+                setTimeout(() => {
+                    if(index < stocks.length){
+                        alphaVantage.getIntraday1mSeriesForAStock(stocks[index], callback);
+                        index++;
+                        getData();            
+                    }
+                }, 3000);
+            })();
+        }
+    }
 }
 
-// encapsulates the logic to regulate the calls to api for multiple stocks 
-// ensures a request is fired once every nth second 
-StockExchange.prototype.getIntraday1MSeriesForAllStocks = function(callback){   
 
-    var stocks = this.getAllStocks();
-    var index = 0;
-    // hold a reference to the stock exchange object to be used inside the closure instead of 'this'    
-    var stockExchange = this;
+var aV1 = new AlphaVantage('3FUAI09JYAO19WCX');
+var aV2 = new AlphaVantage('D11MRXG1OJVDIBYU')
 
-    (function getData(){     
-        setTimeout(() => {
-            if (index < stocks.length){                
-                stockExchange.getIntraday1MSeriesFor(stocks[index], callback)                
-                index++;              
-                getData(); 
-            }
-        }, 3000);         
-    })();    
-}
+var allStocks = aV1.getAllStocks(); 
 
 
-var stockEx = new StockExchange();
+var first100 = allStocks.slice(0, 100);
+var second100 = allStocks.slice(100, 200);
 
-stockEx.getIntraday1MSeriesForAllStocks(function(error, data){
+
+aV1.getIntraday1mSeriesForStocks(first100, function(error, data){
     if(error){
         console.log(error.stock.symbol, error.errorMessage);
     }
@@ -83,3 +116,12 @@ stockEx.getIntraday1MSeriesForAllStocks(function(error, data){
         console.log(data.symbol, data.priceVolumeSeries[0].close);
     }
 });
+
+// aV2.getIntraday1mSeriesForStocks(second100, function(error, data){
+//     if(error){
+//         console.log(error.stock.symbol, error.errorMessage);
+//     }
+//     else{
+//         console.log(data.symbol, data.priceVolumeSeries[0].close);
+//     }
+// });
