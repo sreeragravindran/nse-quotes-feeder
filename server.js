9ff17f97923365466b8206a46831bb92e78d8755
zf58 <sreerag.ravindran@in.tesco.com>
2018-06-19 16:26:48 +0530

    push notification of updated stocks via web socket

--- server.js
+++ server.js
@@ -9,6 +9,13 @@ var app = express();
 //    res.sendFile('./src', {root: __dirname});
 // })
 
+var wss = new WebSocketServer({port: 40510}); 
+wss.on('connection', function (ws) {
+    ws.on('message', function (message) {    
+        //console.log('received: %s', message)
+    })
+})
+
 app.listen(3000, function(){
     console.log("stock quotes service listening on port 3000 !")
 
@@ -16,6 +23,9 @@ app.listen(3000, function(){
       if(latestCandle){
           console.log("updated notification received for ", latestCandle.symbol, latestCandle);
           var viewModel = new viewModels.IchimokuCloudViewModel().mapFrom(latestCandle);  
+          wss.clients.forEach((ws) => {
+            ws.send(viewModel);
+        })   
       }  
     })
 })
